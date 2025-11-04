1b3b0c8ece80a0e7fef83eaf37dceee12c938e94
XuanThao03 <vxuanthao03@gmail.com>
2025-11-04 14:41:08 +0700

    authenticate

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,65 +1,58 @@
-import { useSignIn } from "@clerk/clerk-expo";
-import { Link, useRouter } from "expo-router";
+import { COLORS } from "@/constants/theme";
+import { styles } from "@/styles/auth.styles";
+import { useSSO } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-export default function Page() {
-  const { signIn, setActive, isLoaded } = useSignIn();
+export default function Login() {
+  const { startSSOFlow } = useSSO();
   const router = useRouter();
 
-  const [emailAddress, setEmailAddress] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
-  // Handle the submission of the sign-in form
-  const onSignInPress = async () => {
-    if (!isLoaded) return;
-
-    // Start the sign-in process using the email and password provided
+  const handleGoogleSignIn = async () => {
     try {
-      const signInAttempt = await signIn.create({
-        identifier: emailAddress,
-        password,
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: "oauth_google",
       });
-
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
-      if (signInAttempt.status === "complete") {
-        await setActive({ session: signInAttempt.createdSessionId });
-        router.replace("/");
-      } else {
-        // If the status isn't complete, check why. User might need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2));
+      if (setActive && createdSessionId) {
+        setActive({ session: createdSessionId });
+        router.replace("/(tabs)");
       }
     } catch (err) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
+      console.error("OAuth error:", err);
     }
   };
-
   return (
-    <View>
-      <Text>Sign in</Text>
-      <TextInput
-        autoCapitalize="none"
-        value={emailAddress}
-        placeholder="Enter email"
-        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-      />
-      <TextInput
-        value={password}
-        placeholder="Enter password"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <TouchableOpacity onPress={onSignInPress}>
-        <Text>Continue</Text>
-      </TouchableOpacity>
-      <View style={{ display: "flex", flexDirection: "row", gap: 3 }}>
-        <Link href="/sign-up">
-          <Text>Sign up</Text>
-        </Link>
+    <View style={styles.container}>
+      <View style={styles.brandSection}>
+        <View style={styles.logoContainer}>
+          <Ionicons name="leaf" size={32} color={COLORS.primary} />
+        </View>
+        <Text style={styles.appName}>spotlight</Text>
+        <Text style={styles.tagline}>don't miss anything</Text>
+      </View>
+      <View style={styles.illustrationContainer}>
+        <Image
+          source={require("../../assets/images/auth-bg-2.png")}
+          style={styles.illustration}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.loginSection}>
+        <TouchableOpacity
+          style={styles.googleButton}
+          activeOpacity={0.9}
+          onPress={handleGoogleSignIn}
+        >
+          <View style={styles.googleIconContainer}>
+            <Ionicons name="logo-google" size={20} color={COLORS.surface} />
+          </View>
+          <Text style={styles.googleButtonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <Text style={styles.termsText}>
+          Buy continuing, you agree to our Terms and Privaccy Policy
+        </Text>
       </View>
     </View>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,14 @@
+import InitialLayout from "@/components/InitialLayout";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="index"
-              options={{ title: "Feed", headerShown: false }}
-            />
-          </Stack>
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
